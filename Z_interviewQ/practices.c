d8b5f738d49bf46170736e293dfbaff15cfaed43
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-25 07:23:34 +0530

    -

---
+++ Z_interviewQ/practices.c
@@ -0,0 +1,47 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     printf("A is: %d \n", a);
+//     printf("b is: %d", b);
+// }
+
+// #include<stdio.h>
+// int reverse(int n) {
+//     int rev = 0;
+//     while(n) {
+//         rev = rev * 10 + n % 10;
+//         printf("Rev is:%d \n",rev);
+//         n /= 10;
+//     }
+//     return rev;
+// }
+
+// int main() {
+//     int result = reverse(123);
+//     printf("Reversed number: %d\n", result);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[5] = {10, 85, 30, 40, 89};
+//     int max = arr[0];
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+//         // return max;
+//     }
+//     printf("Max is :%d", max);
+// }
--- function with ard no return/p3.c
+++ function with ard no return/p3.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int add(int n)
 {
-	int i,sum=0;
-	for(i = 1; i < n; i++)  
-                     {
-                            	 if (n%i == 0)  
-                                        		{  
-                                               		sum = sum + i;  
-                                       		  }  
-                      }
-	if (sum == n)  
-                      	printf("Perfect Number");  
-         	  else  
-                      	printf("\nNot Perfect  Number");  
-}  
+	int i, sum = 0;
+	for (i = 1; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	if (sum == n)
+		printf("Perfect Number");
+	else
+		printf("\nNot Perfect  Number");
+}
 int main()
 {
 	int n;
-	printf("Enter a number:");  
-	scanf("%d", &n);      
+	printf("Enter a number:");
+	scanf("%d", &n);
 	add(n);
 }
